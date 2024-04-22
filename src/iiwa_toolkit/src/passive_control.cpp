0ae483330cc24402db9cb98062c62776cd405958
Maxime00 <maxime.gautier@epfl.ch>
2024-04-22 10:43:22 +0200

    added ori ramp up when too far away

--- src/iiwa_toolkit/src/passive_control.cpp
+++ src/iiwa_toolkit/src/passive_control.cpp
@@ -511,16 +511,16 @@ void PassiveControl::computeTorqueCmd(){
         else{
             // Impedance control for orientation torque
 
-            // R_0_d = Utils<double>::quaternionToRotationMatrix(_robot.ee_des_quat);
-            // R_0_tcp = Utils<double>::quaternionToRotationMatrix(_robot.ee_quat);
-            // R_tcp_des = R_0_tcp.transpose() * R_0_d; // R_0_d desired quaternion, R_0_tcp -> ee_quat
-            // R_tcp_des = R_tcp_des - Eigen::MatrixXd::Identity(3,3); // R_0_d desired quaternion, R_0_tcp -> ee_quat
-            // if(!ori_ramp_up && R_tcp_des.norm() > 0.1){
-            //     std::cout << " RAMP UP ORI AGAIN !!: " << R_tcp_des.norm() << std::endl;
-            //     ori_ramp_up = true;
-            //     ori_ramp_up_count = 0;
-            //     get_initial_ee_quat = true;
-            // }
+            R_0_d = Utils<double>::quaternionToRotationMatrix(_robot.ee_des_quat);
+            R_0_tcp = Utils<double>::quaternionToRotationMatrix(_robot.ee_quat);
+            R_tcp_des = R_0_tcp.transpose() * R_0_d; // R_0_d desired quaternion, R_0_tcp -> ee_quat
+            R_tcp_des = R_tcp_des - Eigen::MatrixXd::Identity(3,3); // R_0_d desired quaternion, R_0_tcp -> ee_quat
+            if(!ori_ramp_up && !is_just_velocity && R_tcp_des.norm() > 0.5){
+                std::cout << " RAMP UP ORI AGAIN !!: " << R_tcp_des.norm() << std::endl;
+                ori_ramp_up = true;
+                ori_ramp_up_count = 0;
+                get_initial_ee_quat = true;
+            }
 
             if(ori_ramp_up){ // ramp up des_quat on start to avoid big jump
                 float t_ori = ori_ramp_up_count/max_ramp_up;// calculate t from 0 to 1 depending on time_step
