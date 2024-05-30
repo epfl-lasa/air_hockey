c55060ae58c32dcbf832be7178dd4034f13cfc17
Maxime Gautier <maxime.gautier@epfl.ch>
2024-05-30 12:30:59 +0200

    improved figures for paper and cleaned figures folder

--- python/paper_plots.py
+++ python/paper_plots.py
@@ -12,40 +12,39 @@ from analyse_data import read_airhockey_csv, read_and_clean_data, save_all_figur
 
 
 # Fontsize for axes and titles 
-GLOBAL_FONTSIZE = 30
-AXIS_TICK_FONTSIZE = 20
-
+GLOBAL_FONTSIZE = 40
+AXIS_TICK_FONTSIZE = 30
 PROCESSED_RAW_FOLDER = PATH_TO_DATA_FOLDER + "airhockey_processed/raw/"
 SAVE_FOLDER_FOR_PAPER = "for_paper"   
 
 ## Plots
-def plot_distance_vs_flux(df, colors="iiwa", title="Distance over Flux", with_linear_regression=True, use_mplcursors=True, show_plot=False):
+def plot_distance_vs_flux(df, colors="iiwa", title="Distance over Flux", with_linear_regression=False, use_mplcursors=True, show_plot=False):
     ## use colors input to dtermine color of datapoints
 
     # Plot Flux
-    fig, ax = plt.subplots(1, 1, figsize=(20, 10), sharex=True)
+    plt.figure(figsize=(20, 10))
 
     df_iiwa7 = df[df['IiwaNumber']==7].copy()
     df_iiwa14 = df[df['IiwaNumber']==14].copy()
 
     ## Plot with different colors 
     if colors == "iiwa":
-        ax.scatter(df_iiwa7['HittingFlux'], df_iiwa7['DistanceTraveled'], color='red', alpha=0.5, label='IIWA 7')
-        ax.scatter(df_iiwa14['HittingFlux'], df_iiwa14['DistanceTraveled'], color='blue', alpha=0.5, label='IIWA 14')
+        plt.scatter(df_iiwa7['HittingFlux'], df_iiwa7['DistanceTraveled'], marker='o', s=100, color='orange', alpha=0.8, label='IIWA 7')
+        plt.scatter(df_iiwa14['HittingFlux'], df_iiwa14['DistanceTraveled'], marker='X', s=100, color='blue', alpha=0.8, label='IIWA 14')
 
     elif colors == "config":
         df_cfg1 = df[df['config']==1].copy()
         df_cfg2 = df[df['config']==2].copy()
         # df_cfg1 = df[df['RecSession']=="2024-05-08_16:27:34"].copy()
         # df_cfg2 = df[df['RecSession']=="2024-05-16_13:49:02"].copy()
-        ax.scatter(df_cfg1['HittingFlux'], df_cfg1['DistanceTraveled'], color='green', alpha=0.5, label='Config 1')
-        ax.scatter(df_cfg2['HittingFlux'], df_cfg2['DistanceTraveled'], color='orange', alpha=0.5, label='Config 2')
+        plt.scatter(df_cfg1['HittingFlux'], df_cfg1['DistanceTraveled'], marker='o', s=100,  color='green', alpha=0.8, label='Config 1')
+        plt.scatter(df_cfg2['HittingFlux'], df_cfg2['DistanceTraveled'], marker='X', s=100, color='red', alpha=0.8, label='Config 2')
 
     elif colors == "object":
         df_obj1 = df[df['object']==1].copy()
         df_obj2 = df[df['object']==2].copy()
-        ax.scatter(df_obj1['HittingFlux'], df_obj1['DistanceTraveled'], color='purple', alpha=0.5, label='Object 1')
-        ax.scatter(df_obj2['HittingFlux'], df_obj2['DistanceTraveled'], color='orange', alpha=0.5, label='Object 2')
+        plt.scatter(df_obj1['HittingFlux'], df_obj1['DistanceTraveled'], marker='o', s=100, color='purple', alpha=0.8, label='Object 1')
+        plt.scatter(df_obj2['HittingFlux'], df_obj2['DistanceTraveled'], marker='X', s=100, color='cyan', alpha=0.8, label='Object 2')
 
     ## Add linear regression
     if with_linear_regression: 
@@ -55,7 +54,7 @@ def plot_distance_vs_flux(df, colors="iiwa", title="Distance over Flux", with_li
 
             flux_test = np.linspace(df['HittingFlux'].min(), df['HittingFlux'].max(),100).reshape(-1,1)
             distance_pred = lin_model.predict(flux_test)
-            ax.plot(flux_test,distance_pred,color='black', label='Linear Regression')
+            plt.plot(flux_test,distance_pred,color='black', label='Linear Regression')
         
         elif colors =="config":
             lin_model = LinearRegression()
@@ -63,14 +62,14 @@ def plot_distance_vs_flux(df, colors="iiwa", title="Distance over Flux", with_li
 
             flux_test = np.linspace(df_cfg1['HittingFlux'].min(), df_cfg1['HittingFlux'].max(),100).reshape(-1,1)
             distance_pred = lin_model.predict(flux_test)
-            ax.plot(flux_test,distance_pred,color='green', label='Linear Regression Config 1')
+            plt.plot(flux_test,distance_pred,color='green', label='Linear Regression Config 1')
 
             lin_model2 = LinearRegression()
             lin_model2.fit(df_cfg2['HittingFlux'].values.reshape(-1,1), df_cfg2['DistanceTraveled'].values)
 
             flux_test2 = np.linspace(df_cfg2['HittingFlux'].min(), df_cfg2['HittingFlux'].max(),100).reshape(-1,1)
             distance_pred2 = lin_model2.predict(flux_test2)
-            ax.plot(flux_test2,distance_pred2,color='orange', label='Linear Regression Config 2')
+            plt.plot(flux_test2,distance_pred2,color='red', label='Linear Regression Config 2')
 
         elif colors =="object":
             lin_model = LinearRegression()
@@ -78,14 +77,14 @@ def plot_distance_vs_flux(df, colors="iiwa", title="Distance over Flux", with_li
 
             flux_test = np.linspace(df_obj1['HittingFlux'].min(), df_obj1['HittingFlux'].max(),100).reshape(-1,1)
             distance_pred = lin_model.predict(flux_test)
-            ax.plot(flux_test,distance_pred,color='purple', label='Linear Regression Object 1')
+            plt.plot(flux_test,distance_pred,color='purple', label='Linear Regression Object 1')
 
             lin_model2 = LinearRegression()
             lin_model2.fit(df_obj2['HittingFlux'].values.reshape(-1,1), df_obj2['DistanceTraveled'].values)
 
             flux_test2 = np.linspace(df_obj2['HittingFlux'].min(), df_obj2['HittingFlux'].max(),100).reshape(-1,1)
             distance_pred2 = lin_model2.predict(flux_test2)
-            ax.plot(flux_test2,distance_pred2,color='orange', label='Linear Regression Object 2')
+            plt.plot(flux_test2,distance_pred2,color='cyan', label='Linear Regression Object 2')
 
     # Print some infos
     print(f"Dataset info : \n"
@@ -107,13 +106,22 @@ def plot_distance_vs_flux(df, colors="iiwa", title="Distance over Flux", with_li
         mplcursors.cursor(hover=True).connect('add', lambda sel: sel.annotation.set_text(
             f"IDX: {sel.index} Rec:{df['RecSession'][sel.index]}, hit #{df['HitNumber'][sel.index]}, iiwa{df['IiwaNumber'][sel.index]}"))   
 
-    ax.set_xlabel('Hitting flux [m/s]',fontsize=GLOBAL_FONTSIZE)
-    ax.set_ylabel('Distance Traveled [m]',fontsize=GLOBAL_FONTSIZE)
-    ax.grid(True, alpha=0.5)
-    plt.legend(fontsize=15)
-    plt.title(title ,fontsize=GLOBAL_FONTSIZE)
+    ## Plot parameters
+    plt.xlabel('Hitting flux [m/s]',fontsize=GLOBAL_FONTSIZE)
+    plt.ylabel('Distance Traveled [m]',fontsize=GLOBAL_FONTSIZE)
+
+    # Spines
+    plt.gca().spines['top'].set_visible(False)
+    plt.gca().spines['right'].set_visible(False)
+    plt.gca().spines['left'].set_linewidth(2)  # Set left spine thickness
+    plt.gca().spines['bottom'].set_linewidth(2)
     plt.tick_params(axis='both', which='major', labelsize=AXIS_TICK_FONTSIZE) 
-    fig.tight_layout(rect=(0.01,0.01,0.99,0.99))
+    
+    plt.legend(fontsize=GLOBAL_FONTSIZE)
+
+    # plt.grid(True, alpha=0.5)
+    # plt.title(title ,fontsize=GLOBAL_FONTSIZE)
+    # fig.tight_layout(rect=(0.01,0.01,0.99,0.99))
 
     if show_plot : plt.show()
 
@@ -204,8 +212,13 @@ def plot_object_trajectory_onefig(df, dataset_path="varying_flux_datasets/D1/", 
     # Set axes settings
     for ax in axes:
         ax.set_ylabel('X-axis [m]', fontsize=GLOBAL_FONTSIZE)
-        ax.grid(True, alpha=0.5)
+        # ax.grid(True, alpha=0.5)
         ax.set_ylim(0.39,0.62)
+        # Spines
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        ax.spines['left'].set_linewidth(2)  # Set left spine thickness
+        ax.spines['bottom'].set_linewidth(2)
         ax.tick_params(axis='both', which='major', labelsize=AXIS_TICK_FONTSIZE) 
         
 
@@ -214,7 +227,7 @@ def plot_object_trajectory_onefig(df, dataset_path="varying_flux_datasets/D1/", 
     # leg = fig.legend(loc='center left', bbox_to_anchor=(0.8, 0.6))
     # leg.set_draggable(state=True)
 
-    fig.suptitle(f"Object trajectories seen from above", fontsize=GLOBAL_FONTSIZE)
+    # fig.suptitle(f"Object trajectories seen from above", fontsize=GLOBAL_FONTSIZE)
     # plt.title(f"Object trajectories seen from above", fontsize=GLOBAL_FONTSIZE)
     # plt.tick_params(axis='both', which='major', labelsize=AXIS_TICK_FONTSIZE) 
     
@@ -222,50 +235,50 @@ def plot_object_trajectory_onefig(df, dataset_path="varying_flux_datasets/D1/", 
 
 
 ### Functions to call to recreate paper plots
-def object_traj_plot_for_paper(dataset_to_use='clean'):
+def object_traj_plot_for_paper(use_clean_dataset=True):
 
     print("Plotting object trajectories...")
 
     ### Datasets to use
     csv_fn = "D1_clean" 
 
-    dataset_new_name = "D1-object_traj_plot"
+    new_dataset_name = "D1-object_traj_plot"
 
     ### Read OR clean datasets
-    if dataset_to_use == "raw":
-        clean_df = read_and_clean_data(csv_fn, dataset_name=dataset_new_name,min_flux=0.0, max_flux=1.2, resample=False, save_folder=SAVE_FOLDER_FOR_PAPER, save_clean_df=True)
-    elif dataset_to_use == "clean":
-        clean_df = read_airhockey_csv(fn=dataset_new_name, folder=PATH_TO_DATA_FOLDER + f"airhockey_processed/clean/{SAVE_FOLDER_FOR_PAPER}/")
+    if not use_clean_dataset:
+        clean_df = read_and_clean_data(csv_fn, dataset_name=new_dataset_name,min_flux=0.0, max_flux=1.2, resample=False, save_folder=SAVE_FOLDER_FOR_PAPER, save_clean_df=True)
+    else:
+        clean_df = read_airhockey_csv(fn=new_dataset_name, folder=PATH_TO_DATA_FOLDER + f"airhockey_processed/clean/{SAVE_FOLDER_FOR_PAPER}/")
 
     plot_object_trajectory_onefig(clean_df, dataset_path="varying_flux_datasets/D1/", use_mplcursors=False, selection="selected")
 
-    save_all_figures(folder_name=SAVE_FOLDER_FOR_PAPER, title=dataset_new_name)
+    save_all_figures(folder_name=SAVE_FOLDER_FOR_PAPER, title=new_dataset_name)
 
     plt.show()
 
-def robot_agnostic(dataset_to_use='clean'):
+def robot_agnostic(use_clean_dataset=True):
 
     print("Plotting robot agnostic flux over distance...")
 
     ### Datasets to use
     csv_fn = "D1_clean" 
 
-    dataset_new_name = "D1-robot_agnostic"
+    new_dataset_name = "D1-robot_agnostic"
 
     ### Read OR clean datasets
-    if dataset_to_use == "raw":
+    if not use_clean_dataset :
         # TODO - separate per robot, sample, then recombine to get same number fo poitns per robot
-        clean_df = read_and_clean_data(csv_fn, dataset_name=dataset_new_name, resample=False, n_samples=2000, max_flux=0.85, save_folder=SAVE_FOLDER_FOR_PAPER, save_clean_df=True)
-    elif dataset_to_use == "clean":
-        clean_df = read_airhockey_csv(fn=dataset_new_name, folder=PATH_TO_DATA_FOLDER + f"airhockey_processed/clean/{SAVE_FOLDER_FOR_PAPER}/")
+        clean_df = read_and_clean_data(csv_fn, dataset_name=new_dataset_name, resample=False, n_samples=2000, max_flux=0.85, save_folder=SAVE_FOLDER_FOR_PAPER, save_clean_df=True)
+    else:
+        clean_df = read_airhockey_csv(fn=new_dataset_name, folder=PATH_TO_DATA_FOLDER + f"airhockey_processed/clean/{SAVE_FOLDER_FOR_PAPER}/")
 
-    plot_distance_vs_flux(clean_df, colors="iiwa", with_linear_regression=True, use_mplcursors=False)
+    plot_distance_vs_flux(clean_df, colors="iiwa", with_linear_regression=False, use_mplcursors=False)
 
-    save_all_figures(folder_name=SAVE_FOLDER_FOR_PAPER, title=dataset_new_name)
+    save_all_figures(folder_name=SAVE_FOLDER_FOR_PAPER, title=new_dataset_name)
 
     plt.show()
 
-def object_agnostic(dataset_to_use='clean'):
+def object_agnostic(use_clean_dataset=True):
 
     print("Plotting object agnostic flux over distance...")
 
@@ -273,27 +286,27 @@ def object_agnostic(dataset_to_use='clean'):
     csv_fn = "D1_clean"
     csv_fn2 = "D2_clean" 
 
-    dataset_new_name = "D1-D2-object_agnostic"
+    new_dataset_name = "D1-D2-object_agnostic"
 
     ### Read OR clean datasets
-    if dataset_to_use == "raw":
+    if not use_clean_dataset :
         clean_df = read_and_clean_data(csv_fn, resample=False, n_samples=2000, only_7=True,  save_clean_df=False)
         clean_df2 = read_and_clean_data(csv_fn2, resample=True, n_samples=500, only_7=True,  save_clean_df=False)
 
         ## combine both into one df
         df_combined = restructure_for_agnostic_plots(clean_df, clean_df2, resample=False, parameter="object", 
-                                                     dataset_name=dataset_new_name, save_folder=SAVE_FOLDER_FOR_PAPER, save_new_df=True)
+                                                     dataset_name=new_dataset_name, save_folder=SAVE_FOLDER_FOR_PAPER, save_new_df=True)
 
-    elif dataset_to_use == "clean":
-        df_combined = read_airhockey_csv(fn=dataset_new_name, folder=PATH_TO_DATA_FOLDER + f"airhockey_processed/clean/{SAVE_FOLDER_FOR_PAPER}/")
+    else :
+        df_combined = read_airhockey_csv(fn=new_dataset_name, folder=PATH_TO_DATA_FOLDER + f"airhockey_processed/clean/{SAVE_FOLDER_FOR_PAPER}/")
         
-    plot_distance_vs_flux(df_combined, colors="object", with_linear_regression=True, use_mplcursors=False)
+    plot_distance_vs_flux(df_combined, colors="object", with_linear_regression=False, use_mplcursors=False)
 
-    save_all_figures(folder_name=SAVE_FOLDER_FOR_PAPER, title=dataset_new_name)
+    save_all_figures(folder_name=SAVE_FOLDER_FOR_PAPER, title=new_dataset_name)
 
     plt.show()
 
-def config_agnostic(dataset_to_use='clean'):
+def config_agnostic(use_clean_dataset=True):
     
     print("Plotting config agnostic flux over distance...")
 
@@ -301,10 +314,10 @@ def config_agnostic(dataset_to_use='clean'):
     csv_fn = "D1_clean"
     csv_fn2 = "D3_clean" 
 
-    dataset_new_name = "D1-D3-config_agnostic"
+    new_dataset_name = "D1-D3-config_agnostic"
 
     ### Read OR clean datasets
-    if dataset_to_use == "raw":
+    if not use_clean_dataset :
 
         ## Read and clean datasets
         clean_df = read_and_clean_data(csv_fn, resample=False, max_flux=1.0, only_7=True, save_clean_df=False)
@@ -316,15 +329,15 @@ def config_agnostic(dataset_to_use='clean'):
         
         ## combine both into one df
         df_combined = restructure_for_agnostic_plots(clean_df, clean_df2, resample=False, parameter="config", 
-                                                     dataset_name=dataset_new_name, save_folder=SAVE_FOLDER_FOR_PAPER, save_new_df=True)
+                                                     dataset_name=new_dataset_name, save_folder=SAVE_FOLDER_FOR_PAPER, save_new_df=True)
 
-    elif dataset_to_use == "clean":
-        df_combined = read_airhockey_csv(fn=dataset_new_name, folder=PATH_TO_DATA_FOLDER + f"airhockey_processed/clean/{SAVE_FOLDER_FOR_PAPER}/")
-        # df_combined = read_airhockey_csv(fn=dataset_new_name, folder=PATH_TO_DATA_FOLDER + f"airhockey_processed/clean/KL_div-config_agnostic/")
+    else:
+        df_combined = read_airhockey_csv(fn=new_dataset_name, folder=PATH_TO_DATA_FOLDER + f"airhockey_processed/clean/{SAVE_FOLDER_FOR_PAPER}/")
+        # df_combined = read_airhockey_csv(fn=new_dataset_name, folder=PATH_TO_DATA_FOLDER + f"airhockey_processed/clean/KL_div-config_agnostic/")
         
-    plot_distance_vs_flux(df_combined, colors="config", with_linear_regression=True, use_mplcursors=False)
+    plot_distance_vs_flux(df_combined, colors="config", with_linear_regression=False, use_mplcursors=False)
 
-    save_all_figures(folder_name=SAVE_FOLDER_FOR_PAPER, title=dataset_new_name)
+    save_all_figures(folder_name=SAVE_FOLDER_FOR_PAPER, title=new_dataset_name)
 
     plt.show()
 
@@ -336,8 +349,8 @@ if __name__== "__main__" :
     ## Tune parameters inside each respective function
     ## Use 'raw' dataset to sample different datapoints for plots
 
-    # object_traj_plot_for_paper('clean')
-    # robot_agnostic(dataset_to_use='clean')
-    # object_agnostic(dataset_to_use='clean')
-    config_agnostic(dataset_to_use="raw")
+    # object_traj_plot_for_paper(use_clean_dataset=True)
+    # robot_agnostic(use_clean_dataset=True)
+    # object_agnostic(use_clean_dataset=True)
+    config_agnostic(use_clean_dataset=True)
 
